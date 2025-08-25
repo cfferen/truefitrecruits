a81e535f5e7cc47af9ca1ce6a086dee9e8c052a1
cfferen <cferen@icloud.com>
2025-08-25 12:25:54 -0400

    Create page.tsx

---
+++ app/resources/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = { title: "Resources | TrueFit Recruits" };
+
+export default function ResourcesPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Resources</h1>
+      <ul>
+        <li>Coach checklists & evaluation templates</li>
+        <li>Prospect profile best practices</li>
+        <li>Film guidelines and highlight tips</li>
+      </ul>
+    </main>
+  );
+}
