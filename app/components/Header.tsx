bdb0c5df2c17ded52861b169acea5678b614eb93
cfferen <cferen@icloud.com>
2025-08-25 12:56:36 -0400

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,17 @@
-<div className="nav-menu">
-  <Link href="/sports/lacrosse/men">Men’s Lacrosse</Link>
-  <Link href="/sports/lacrosse/women">Women’s Lacrosse</Link>
-  <Link href="/sports/football/men">Men’s Football</Link>
-  <Link href="/sports/soccer/men">Men’s Soccer</Link>
-  <Link href="/sports/soccer/women">Women’s Soccer</Link>
-</div>
+"use client";
+
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <nav className="nav-menu">
+      <ul>
+        <li><Link href="/sports/lacrosse/men">Men’s Lacrosse</Link></li>
+        <li><Link href="/sports/lacrosse/women">Women’s Lacrosse</Link></li>
+        <li><Link href="/sports/football/men">Men’s Football</Link></li>
+        <li><Link href="/sports/soccer/men">Men’s Soccer</Link></li>
+        <li><Link href="/sports/soccer/women">Women’s Soccer</Link></li>
+      </ul>
+    </nav>
+  );
+}
